bb3cf218c488df2f79ae5bedc1785509fbfa28ec
Coconut Beach <hello@coconutbeachkohphangan.com>
2026-07-27 19:03:27 +0700

    fix: restore two homepage hero images

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 const heroImages = [
   "https://media.coconut.holiday/Atmosphere/18f18e1e-2e59-4578-a72d-57839a1af04a.jpg",
   "https://media.coconut.holiday/Atmosphere/69633d3f-afb1-4a3c-9712-a5051117a3b2.png",
+  "https://media.coconut.holiday/Atmosphere/IMG_2425.jpeg",
+  "https://media.coconut.holiday/Atmosphere/IMG_2578.jpeg",
 ];
 
 export default function HomeHero() {
