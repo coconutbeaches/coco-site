4966b5676e16c7370c5ac481f2fa026cf12a0d4b
Coconut Beach <hello@coconutbeachkohphangan.com>
2026-08-09 18:37:58 +0700

    style: load modern date picker styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./hero-overlay.css";
 import "./mobile-fixes.css";
 import "./search-results.css";
 import "./availability-font.css";
+import "./date-picker.css";
 
 const pacifico = Pacifico({
   weight: "400",
